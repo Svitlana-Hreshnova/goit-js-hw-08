5c17dd5c9db80909fa0eebe0afafea48c7c39794
Svitlana-Hreshnova <133237028+Svitlana-Hreshnova@users.noreply.github.com>
2023-10-07 12:10:08 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,60 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from './gallery-items.js';
+
+let isModalOpen = false;
+
+const galleryList = document.querySelector('.gallery');
+
+function createGalleryItemMarkup(item) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+  `;
+}
+
+const galleryItemsMarkup = galleryItems.map(createGalleryItemMarkup).join('');
+
+galleryList.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+galleryList.addEventListener('click', handleGalleryItemClick);
+
+function handleGalleryItemClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const originalImageUrl = event.target.dataset.source;
+
+  const modal = basicLightbox.create(
+    `<img src="${originalImageUrl}" width="800" height="600">`
+  );
+
+  modal.show();
+
+  isModalOpen = true;
+
+  document.addEventListener('keydown', handleKeyPress);
+
+  function handleKeyPress(event) {
+    if (event.key === 'Escape' && isModalOpen) {
+      modal.close();
+      isModalOpen = false;
+      document.removeEventListener('keydown', handleKeyPress);
+    }
+  }
+}
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const storageKey = 'feedback-form-state';
+
+const saveFormState = () => {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(storageKey, JSON.stringify(formData));
+};
+
+const loadFormState = () => {
+  const savedData = localStorage.getItem(storageKey);
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    emailInput.value = formData.email || '';
+    messageInput.value = formData.message || '';
+  }
+};
+
+const throttledSaveFormState = throttle(saveFormState, 500);
+
+loadFormState();
+
+form.addEventListener('input', () => {
+  throttledSaveFormState();
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  localStorage.removeItem(storageKey);
+  emailInput.value = '';
+  messageInput.value = '';
+  console.log({
+    email: emailInput.value,
+    message: messageInput.value,
+  });
+});
 
